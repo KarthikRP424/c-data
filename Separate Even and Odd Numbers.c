faef52793b3740ff438f236b960f419fde7b69be
KarthikRP424 <karthukarthik242@gmail.com>
2025-09-19 19:17:35 +0530

    Add files via upload

---
+++ Separate Even and Odd Numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d numbers:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nEven numbers: ");
+    for(i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    printf("\nOdd numbers: ");
+    for(i = 0; i < n; i++) {
+        if(arr[i] % 2 != 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
